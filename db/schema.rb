456ce9d441af14efe90d1d04618d50d0cd6011a1
kobayashihotaru <blue02.sh@icloud.com>
2020-04-26 09:48:52 +0000

    マイグレーション追加(ニックネーム,年齢,性別)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_24_150145) do
+ActiveRecord::Schema.define(version: 2020_04_26_094228) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
@@ -18,6 +18,10 @@ ActiveRecord::Schema.define(version: 2020_04_24_150145) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.string "image_name"
+    t.string "sex"
+    t.integer "age", limit: 1
+    t.string "nickname"
   end
 
 end
