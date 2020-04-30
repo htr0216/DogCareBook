5ec12513d33da33888df108e6e2725976ab74a0e
kobayashihotaru <blue02.sh@icloud.com>
2020-04-30 03:54:49 +0000

    topicsコントローラ作成、ルーティング追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
   
+  get 'topics/new'
   get 'sessions/new'
   root 'pages#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
   resource :user
+  resources :topics
   
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
