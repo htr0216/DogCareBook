e53f0055d8f4122bc024db892c0bfa8ea2941b08
htr0216 <blue02.sh@icloud.com>
2020-05-15 01:09:15 +0900

    ナビゲーションヘッダーactive設定をした。

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def navigation_item_class(controller, action = nil, except: nil)
+    # コントローラ名が一致しない場合は、nilを返す。
+    return nil unless params[:controller] == controller 
+    # アクションが指定されていて一致しない場合は、nilを返す。
+    return nil if action != nil && params[:action] != action
+    # アクション名が除外するものだった場合、nilを返す。
+    return nil if params[:action] == except
+    "active"
+  end
 end
