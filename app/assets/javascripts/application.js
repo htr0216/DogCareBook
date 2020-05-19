851179fc6b1055591424c06f066f0c045150e0c4
htr0216 <blue02.sh@icloud.com>
2020-05-19 12:31:26 +0900

    fullcalendar画面viewのために、application.jsへ追記した。

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,3 +17,85 @@
 
 //= require bootstrap-sprockets
 //= require_tree .
+
+//= require jquery
+//= require moment
+//= require fullcalendar
+
+$(function () {
+  // 画面遷移を検知
+  $(document).on('turbolinks:load', function () {
+      if ($('#calendar').length) {
+
+          function Calendar() {
+              return $('#calendar').fullCalendar({
+              });
+          }
+          function clearCalendar() {
+              $('#calendar').html('');
+          }
+
+          $(document).on('turbolinks:load', function () {
+              Calendar();
+          });
+          $(document).on('turbolinks:before-cache', clearCalendar);
+
+          //events: '/events.json', 以下に追加
+          $('#calendar').fullCalendar({
+              events: '/events.json',
+              //カレンダー上部を年月で表示させる
+              titleFormat: 'YYYY年 M月',
+              //曜日を日本語表示
+              dayNamesShort: ['日', '月', '火', '水', '木', '金', '土'],
+              //ボタンのレイアウト
+              header: {
+                  left: '',
+                  center: 'title',
+                  right: 'today,prev,next'
+              },
+              //終了時刻がないイベントの表示間隔
+              defaultTimedEventDuration: '03:00:00',
+              buttonText: {
+                  prev: '<',
+                  next: '>',
+                  prevYear: '前年',
+                  nextYear: '翌年',
+                  today: '今日',
+                  month: '月',
+                  week: '週',
+                  day: '日'
+              },
+              height: 960,
+              // Drag & Drop & Resize
+              editable: true,
+              slotEventOverlap: false,
+              //イベントの時間表示を２４時間に
+              timeFormat: "HH:mm",
+              //イベントの色を変える
+              eventColor: '#87cefa',
+              //イベントの文字色を変える
+              eventTextColor: '#000000',
+              eventRender: function(event, element) {
+                  element.css("font-size", "0.8em");
+                  console.log(event);
+                  if (event.category === "food") {
+                      //foodだったら
+                      element.css("background-color", "#fda832");
+                  } else if (event.category === "health") {
+                      //healthだったら
+                      element.css("background-color", "#93df96");
+                  } else if (event.category === "medicine") {
+                      //medicineだったら
+                      element.css("background-color", "#f1d2ed");
+                  } else if (event.category === "hospital") {
+                      //hospitalだったら
+                      element.css("background-color", "#a7e4e5");
+                  }
+
+                  //element.css("background-color", "#fda832");
+                  element.css("padding", "5px");
+              }
+          });
+      }
+  });
+});
