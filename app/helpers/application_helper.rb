c7f025c70874f1dc3cfd5ac59daa4ec3f5bb8bd7
htr0216 <blue02.sh@icloud.com>
2020-05-24 00:08:06 +0900

    改行ありで入力した値をきちんと表示させる、記録した時間をi18nのformatを使用して表示されるようにした。

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,17 @@
 module ApplicationHelper
   def navigation_item_class(controller, action = nil, except: nil)
     # コントローラ名が一致しない場合は、nilを返す。
-    return nil unless params[:controller] == controller 
+    return nil unless params[:controller] == controller
     # アクションが指定されていて一致しない場合は、nilを返す。
     return nil if action != nil && params[:action] != action
     # アクション名が除外するものだった場合、nilを返す。
     return nil if params[:action] == except
     "active"
+  end
+
+  def hbr(target)
+    target = html_escape(target)
+    target = target.gsub(/\r\n|\r|\n/, "<br />")
+    target.html_safe
   end
 end
