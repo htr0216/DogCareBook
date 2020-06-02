90302f5aea9dcdc44af36a4d8f19486f048d38e7
htr0216 <blue02.sh@icloud.com>
2020-06-02 19:48:20 +0900

    画像アップロード先を変更した。

---
+++ config/initializers/carrierwave.rb
@@ -0,0 +1,13 @@
+unless Rails.env.development? || Rails.env.test?
+  CarrierWave.configure do |config|
+    config.fog_credentials = {
+      provider: 'AWS',
+      aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+      aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+      region: 'us-east-1'
+    }
+
+    config.fog_directory  = 'dog-care-book'
+    config.cache_storage = :fog
+  end
+end
